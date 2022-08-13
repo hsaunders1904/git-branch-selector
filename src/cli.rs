7f669f2db67950e6703f531b8cd9e218e4d09f25
Harry Saunders <33317174+hsaunders1904@users.noreply.github.com>
2022-08-14 00:56:01 +0100

    Add CLI tests

--- src/cli.rs
+++ src/cli.rs
@@ -1,10 +1,5 @@
 use clap::Parser;
 
-#[derive(clap::ValueEnum, Clone, Debug)]
-pub enum Action {
-    Delete,
-}
-
 #[derive(Parser, Debug)]
 #[clap(author, version)]
 pub struct Args {
@@ -24,4 +19,41 @@ fn get_working_dir() -> String {
 
 pub fn parse_args(args: impl Iterator<Item = String>) -> Args {
     Args::parse_from(args)
+}
+
+#[cfg(test)]
+mod tests {
+    mod parse_args {
+        use crate::cli::parse_args;
+
+        macro_rules! to_string_iter {
+            ($element: expr) => {{
+                $element.iter().map(|s| s.to_string())
+            }};
+        }
+
+        #[test]
+        fn git_dir_is_first_positional_arg() {
+            let cli_args = to_string_iter!(["", "/some/dir"]);
+
+            let args = parse_args(cli_args);
+
+            assert_eq!(args.git_dir, "/some/dir");
+        }
+
+        #[test]
+        fn git_dir_is_working_dir_by_default() {
+            let cli_args = to_string_iter!([""]);
+
+            let args = parse_args(cli_args);
+
+            assert_eq!(
+                args.git_dir,
+                std::env::current_dir()
+                    .expect("Couldn't get working directory.")
+                    .to_str()
+                    .expect("Couldn't convert working directory to string.")
+            );
+        }
+    }
 }
